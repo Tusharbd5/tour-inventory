90d08877e8eb1ffa76a77278729d07fdd3d0e5db
Tusharbd5 <tushardebhata24@gmail.com>
2022-09-20 20:52:34 +0600

    live server added

--- server.js
+++ server.js
@@ -5,9 +5,16 @@ const colors = require("colors");
 const app = require("./app");
 
 // Database Connection
-mongoose.connect(process.env.DATABASE_LOCAL).then(() => {
-    console.log('Database Connection is Successful');
-})
+// mongoose.connect(process.env.DATABASE_LOCAL).then(() => {
+//     console.log('Database Connection is Successful');
+// })
+try {
+    mongoose.connect('mongodb+srv://tusharbd5:tanjim.hasan1@cluster0.atokzk2.mongodb.net/?retryWrites=true&w=majority').then(() => {
+        console.log('Database Connection is Successful');
+    })
+} catch (error) {
+
+}
 
 // Server
 const port = process.env.PORT || 8080;
